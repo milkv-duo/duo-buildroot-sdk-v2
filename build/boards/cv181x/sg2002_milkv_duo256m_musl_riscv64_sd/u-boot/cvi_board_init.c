aa8a60298ef8a2502cc0b6f787c53924905ac888
carbon <carbon@milkv.io>
2024-07-30 17:10:55 +0800

    duo256m and duos support cameras
    Signed-off-by: carbon <carbon@milkv.io>

--- build/boards/cv181x/sg2002_milkv_duo256m_musl_riscv64_sd/u-boot/cvi_board_init.c
+++ build/boards/cv181x/sg2002_milkv_duo256m_musl_riscv64_sd/u-boot/cvi_board_init.c
@@ -8,6 +8,12 @@ int cvi_board_init(void)
 	// LED
 	PINMUX_CONFIG(PWR_GPIO2, PWR_GPIO_2);
 
+	// Camera
+	PINMUX_CONFIG(PAD_MIPI_TXM1, IIC2_SDA);    // GP10
+	PINMUX_CONFIG(PAD_MIPI_TXP1, IIC2_SCL);    // GP11
+	PINMUX_CONFIG(PAD_MIPI_TXP0, CAM_MCLK0);   // Sensor MCLK
+	PINMUX_CONFIG(PAD_MIPI_TXP2, XGPIOC_17);   // Sensor RESET
+
 	set_rtc_register_for_power();
 
 	return 0;
