bf37d6a3d6afd0ac57bc552d0842f625b6e978a3
carbon <carbon@milkv.io>
2024-08-14 14:01:38 +0800

    device add board milkv-duo-musl-riscv64-sd
    Signed-off-by: carbon <carbon@milkv.io>

---
+++ device/generic/rootfs_overlay/duo/mnt/system/usb-ncm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+/etc/uhubon.sh device >> /tmp/ncm.log 2>&1
+/etc/run_usb.sh probe ncm >> /tmp/ncm.log 2>&1
+if test -e /usr/bin/burnd; then
+  /etc/run_usb.sh probe acm >> /tmp/ncm.log 2>&1
+fi
+/etc/run_usb.sh start ncm >> /tmp/ncm.log 2>&1
+
+sleep 0.5
+ifconfig usb0 192.168.42.1
+
+count=`ps | grep dnsmasq | grep -v grep | wc -l`
+if [ ${count} -lt 1 ] ;then
+  echo "/etc/init.d/S80dnsmasq start" >> /tmp/ncm.log 2>&1
+  /etc/init.d/S80dnsmasq start >> /tmp/ncm.log 2>&1
+fi
+
+sleep 2
+mkdir -p /lib/firmware
+if test -e /usr/bin/burnd; then
+  burnd &
+  if test -e /lib/firmware/arduino.elf; then
+    sleep 2
+    echo stop  > /sys/class/remoteproc/remoteproc0/state
+    echo start > /sys/class/remoteproc/remoteproc0/state
+  fi
+fi
