2738c48cc9b1cd0d941c52a597eccaa2b3e29244
wuxy-tpu <577045714@qq.com>
2024-09-22 16:08:45 +0800

    updata README updata cmakelist.txt

---
+++ cvibuilder/CMakeFiles/cvibuilder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvibuilder"
+  "include/cvibuilder/cvimodel_generated.h"
+  "include/cvibuilder/parameter_generated.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cvibuilder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
