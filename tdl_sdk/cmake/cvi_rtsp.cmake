1008a3a08f29f7e81ffdaf90055c64f36e88b524
carbon <carbon@milkv.io>
2024-09-24 18:56:16 +0800

    tdl_sdk uses local non-git cvi_rtsp
    Signed-off-by: carbon <carbon@milkv.io>

--- tdl_sdk/cmake/cvi_rtsp.cmake
+++ tdl_sdk/cmake/cvi_rtsp.cmake
@@ -16,33 +16,67 @@ else()
 endif()
 
 if(EXISTS $ENV{TOP_DIR}/cvi_rtsp)
-  set(PROJECT_URL "file://$ENV{TOP_DIR}/cvi_rtsp")
+  set(PROJECT_URL "$ENV{TOP_DIR}/cvi_rtsp")
 else()
   set(PROJECT_URL "https://github.com/sophgo/cvi_rtsp.git")
 endif()
 
-set(SOURCE_DIR ${BUILD_DOWNLOAD_DIR}/cvi_rtsp/src/cvi_rtsp)
+message(STATUS "PROJECT_URL for cvi_rtsp is: ${PROJECT_URL}")
+
+set(SOURCE_DIR ${BUILD_DOWNLOAD_DIR}/cvi_rtsp)
 
 if(NOT EXISTS "$ENV{TOP_DIR}/cvi_rtsp/src/libcvi_rtsp.so")
   if(NOT EXISTS ${SOURCE_DIR}/src/libcvi_rtsp.so)
-    ExternalProject_Add(cvi_rtsp
-      GIT_REPOSITORY ${PROJECT_URL}
-      PREFIX ${BUILD_DOWNLOAD_DIR}/cvi_rtsp
-      BUILD_COMMAND CROSS_COMPILE=${TC_PATH}${CROSS_COMPILE} SDK_VER=${RTSP_SDK_VER} ./build.sh
-      CONFIGURE_COMMAND ""
-      INSTALL_COMMAND ""
-      BUILD_IN_SOURCE true
-      BUILD_BYPRODUCTS <SOURCE_DIR>/src/libcvi_rtsp.so
-    )
-    ExternalProject_Get_property(cvi_rtsp SOURCE_DIR)  
-    message("Content downloaded to ${SOURCE_DIR}")
+    if(EXISTS "${PROJECT_URL}")
+      message(STATUS "Copying cvi_rtsp from ${PROJECT_URL} to ${BUILD_DOWNLOAD_DIR}")
+      execute_process(
+        COMMAND cp -r ${PROJECT_URL} ${BUILD_DOWNLOAD_DIR}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        RESULT_VARIABLE result
+      )
+
+      if(result EQUAL 0)
+        message(STATUS "cvi_rtsp copied to ${BUILD_DOWNLOAD_DIR}")
+        if(EXISTS "${SOURCE_DIR}/build.sh")
+          message(STATUS "build.sh exists and is executable")
+        endif()
+        add_custom_command(
+          OUTPUT ${SOURCE_DIR}/src/libcvi_rtsp.so
+          COMMAND CROSS_COMPILE=${TC_PATH}${CROSS_COMPILE} SDK_VER=${RTSP_SDK_VER} ./build.sh
+          WORKING_DIRECTORY ${BUILD_DOWNLOAD_DIR}/cvi_rtsp
+          VERBATIM
+          COMMENT "Building libcvi_rtsp from copied files"
+        )
+
+        add_custom_target(build_cvi_rtsp ALL DEPENDS ${SOURCE_DIR}/src/libcvi_rtsp.so)
+      endif()
+
+    elseif(PROJECT_URL MATCHES "^https://")
+      ExternalProject_Add(cvi_rtsp
+        GIT_REPOSITORY ${PROJECT_URL}
+        PREFIX ${BUILD_DOWNLOAD_DIR}/cvi_rtsp
+        BUILD_COMMAND CROSS_COMPILE=${TC_PATH}${CROSS_COMPILE} SDK_VER=${RTSP_SDK_VER} ./build.sh
+        CONFIGURE_COMMAND ""
+        INSTALL_COMMAND ""
+        BUILD_IN_SOURCE true
+        BUILD_BYPRODUCTS <SOURCE_DIR>/src/libcvi_rtsp.so
+      )
+      ExternalProject_Get_property(cvi_rtsp SOURCE_DIR)
+      message("Content downloaded to ${SOURCE_DIR}")
+    endif()
+
   endif()
 else()
   set(SOURCE_DIR $ENV{TOP_DIR}/cvi_rtsp)
 endif()
 
+message(STATUS "cvi_rtsp SOURCE_DIR: ${SOURCE_DIR}")
+
 set(CVI_RTSP_LIBPATH ${SOURCE_DIR}/src/libcvi_rtsp.so)
 set(CVI_RTSP_INCLUDE ${SOURCE_DIR}/include/cvi_rtsp)
+
+message(STATUS "CVI_RTSP_LIBPATH: ${CVI_RTSP_LIBPATH}")
+message(STATUS "CVI_RTSP_INCLUDE: ${CVI_RTSP_INCLUDE}")
 
 set(RTSP_PATH ${CMAKE_INSTALL_PREFIX}/sample/3rd/rtsp)
 install(FILES ${CVI_RTSP_LIBPATH} DESTINATION ${RTSP_PATH}/lib)
