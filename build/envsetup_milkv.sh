4f4e7e89559b7bffc62a7a916b25560ae49514ee
carbon <carbon@milkv.io>
2024-12-09 10:54:20 +0800

    install ai samples for duo cv1800b
    Signed-off-by: carbon <carbon@milkv.io>

--- build/envsetup_milkv.sh
+++ build/envsetup_milkv.sh
@@ -352,7 +352,7 @@ function build_sdk()
   # copy so
   cp -a "$SDK_INSTALL_PATH"/lib/*.so* "$SYSTEM_OUT_DIR"/lib/
   # copy sample_xxx
-  if [[ "$CHIP_ARCH" != CV180X ]] && [[ "$1" = ai ]]; then
+  if [[ "$1" = ai ]]; then
     mkdir -p "$SYSTEM_OUT_DIR"/usr/bin/"$1"
     cp -a "$SDK_INSTALL_PATH"/bin/sample_* "$SYSTEM_OUT_DIR"/usr/bin/"$1"
     cp -a "${SDK_INSTALL_PATH}/sample/3rd/rtsp/lib/libcvi_rtsp.so" "$SYSTEM_OUT_DIR"/lib/
